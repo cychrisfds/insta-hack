ffa21d27044e3c5f94e0fec0a002e6e87252b012
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-01-25 21:36:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-apt-get python
-apt-get python2 
-apt-get python3
-apt-get git
-apt-get wget
+apt-get install python
+apt-get install python2 
+apt-get install python3
+apt-get install git
+apt-get install wget
 apt-get upgrade && update
 apt-get npm 
