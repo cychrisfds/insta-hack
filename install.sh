4a519786b61d67d311426c0fae1a724b8a00acdf
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-01-25 21:37:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
+
+PREPARAR
+
+APONTAR
+
+JAAAA
+
 apt-get install python
 apt-get install python2 
 apt-get install python3
 apt-get install git
 apt-get install wget
 apt-get upgrade && update
-apt-get npm 
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
